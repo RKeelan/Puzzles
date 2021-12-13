2863fc23ca0b3fbefe01a3961f3e52d9333bf19d
R.Keelan <R.Keelan@gmail.com>
2021-12-13 00:25:31 -0500

    Add Puzzles solution with Euler F# project and a solution to problem 1

---
+++ Euler/000/10/Euler1.fs
@@ -0,0 +1,11 @@
+﻿module Euler1
+
+open System
+
+let isMultipleOfThreeOrFive n = 
+    if ((n % 3) = 0) || ((n% 5) = 0)
+    then n
+    else 0
+
+let sumOfMultiplesBelow n =
+    [1..n-1] |> List.map isMultipleOfThreeOrFive |> List.sum
---
+++ Euler/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open Euler1
+
+[<EntryPoint>]
+let main argv =
+    let number = sumOfMultiplesBelow 1000
+    printf "%d" number
+    0 // return an integer exit code
