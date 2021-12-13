4e5e1b95a406213726cb9705d97084c78374327e
R.Keelan <R.Keelan@gmail.com>
2021-12-13 18:24:03 -0500

    Add Euler5

---
+++ Euler/000/10/Euler5.fs
@@ -0,0 +1,32 @@
+﻿module Euler5
+
+(*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+https://projecteuler.net/problem=5
+*)
+
+open System
+open System.Linq
+
+let rec isDivisibleByList n list =
+    //printfn $"{n}, %A{list}"
+    match list with
+    | [] -> true
+    | _ -> if ((n % list.Head) = 0) then isDivisibleByList n list.Tail else false
+
+
+
+//[<EntryPoint>]
+let main argv =
+    let divisors = [2 .. 20]
+
+    // Only need to try every 10th number, because we know the numbers between won't be evenly
+    // divisible by 10
+    let numbers = Seq.initInfinite (fun n -> n*10 + 10)
+    let _ = numbers.Take 10 |> Seq.toList |> printfn "%A" 
+    let smallestMulitple = numbers |> Seq.filter (fun n -> isDivisibleByList n divisors) |> Seq.head
+
+    printfn $"{smallestMulitple}"
+    0
