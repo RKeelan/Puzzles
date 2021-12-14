2ae01fa5fe0cd7a9343e3e1e45842de41d1cc46b
R.Keelan <R.Keelan@gmail.com>
2021-12-14 18:34:56 -0500

    Add Util functions for generating prime numbers
    - Add Util.nextPrime, Util.primes (an infinite sequence of prime numbers), and Util.nthPrime, with unit tests
    - Replace Util.exceptSorted with a non-tail-recursive version

--- Euler/Util.fs
+++ Euler/Util.fs
@@ -1,9 +1,16 @@
 ﻿module Util
 
+open System
+open System.Linq
+
+// Math Helpers -----------------------------------------------------------------------------------
+
 let ceilSqrt n = int (ceil (sqrt (float n)))
 let floorSqrt n = int (floor (sqrt (float n)))
 
 let isDivisible n d = ((n % d) = 0)
+
+let isEven n = isDivisible n 2
 
 let rec isDivisibleByAll n list =
     match list with
@@ -15,6 +22,8 @@ let rec isDivisibleByAny n list =
     | [] -> false
     | _ -> if (isDivisible n list.Head) then true else  isDivisibleByAny n list.Tail
 
+// Primes -----------------------------------------------------------------------------------------
+
 let isPrime p =
     match p with
     | 1 | 4 | 6 | 8 | 10 -> false
@@ -24,10 +33,35 @@ let isPrime p =
         //printfn "%A" divisors
         not (isDivisibleByAny p divisors)
 
+let nextPrime start =
+    // RK 14-Dec-2021: Use the match to handle various corner cases
+    match start with
+    | i when i < 2 -> 2
+    | 2 -> 3
+    | _ ->
+        // RK 14-Dec-2021: This is a bit controverial, but I feel like calling "nextPrime" on a prime
+        // number should return the next prime after that number, not that number
+        let firstNonPrime = if isPrime start then (start + 1) else start
+        let sequenceStart = if isEven firstNonPrime then (firstNonPrime + 1) else firstNonPrime
+        let candidates = Seq.initInfinite (fun n -> n*2 + sequenceStart)
+        Seq.head (candidates |> Seq.filter (fun n -> isPrime n))
+
+let primes =
+    2 // First prime
+    |> Seq.unfold (fun state -> Some(state, nextPrime state))
+    
+
+let rec nthPrime n = Seq.head(primes.Skip(n - 1))
+
+// Miscellaenous ----------------------------------------------------------------------------------
+
+// From https://stackoverflow.com/a/1506343
 let rec exceptSorted a b =
-    match (a, b) with
-    | [], x | x, [] -> x
-    | x::xs, y::ys ->
-        if x < y then x :: exceptSorted xs (y::ys)
-        elif x > y then y :: exceptSorted (x::xs) ys
-        else exceptSorted xs ys
+    let rec loop acc a b =
+        match (a, b) with
+        | [], x | x, [] -> (List.rev acc) @ x
+        | x::xs, y::ys ->
+            if x < y then loop (x::acc) xs (y::ys)
+            elif x > y then loop (y::acc) (x::xs) ys
+            else loop acc xs ys
+    loop [] a b
