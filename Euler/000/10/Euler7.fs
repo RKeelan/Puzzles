6adff96d5e28a6ca17df37bf956e3bd672e83e02
R.Keelan <R.Keelan@gmail.com>
2021-12-14 18:39:00 -0500

    Add Euler7

---
+++ Euler/000/10/Euler7.fs
@@ -0,0 +1,17 @@
+﻿module Euler7
+
+(*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+https://projecteuler.net/problem=7
+*)
+
+
+open Util
+
+//[<EntryPoint>]
+let main argv =
+    let n = 10001
+    printfn $"Prime #{n} = { Util.nthPrime n}"
+    0
