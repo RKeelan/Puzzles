c3610075a30c129414c821fed4385f47b47e5faa
R.Keelan <R.Keelan@gmail.com>
2021-12-19 11:40:10 -0500

    Add Time module

---
+++ Euler/Time.fs
@@ -0,0 +1,79 @@
+﻿module Time
+
+open System
+open Util
+
+// Types ------------------------------------------------------------------------------------------
+
+type Day =
+    | Mon
+    | Tues
+    | Wed
+    | Thurs
+    | Fri
+    | Sat
+    | Sund
+
+type Month =
+    | Jan
+    | Feb
+    | Mar
+    | Apr
+    | May
+    | Jun
+    | Jul
+    | Aug
+    | Sep
+    | Oct
+    | Nov
+    | Dec
+
+// Year Functions ---------------------------------------------------------------------------------
+
+let isLeapYear (year:int) : bool =
+    if not(Util.isDivisible year 4) then false
+    else
+        if Util.isDivisible year 400 then true
+        elif Util.isDivisible year 100 then false
+        else true
+
+let daysInYear year = if isLeapYear year then 366 else 365
+
+// Month Functions --------------------------------------------------------------------------------
+
+let Months = [Jan;Feb;Mar;Apr;May;Jun;Jul;Aug;Sep;Oct;Nov;Dec]
+
+let daysInMonth month year =
+    match month with 
+    | Jan -> 31
+    | Feb -> if isLeapYear year then 29 else 28
+    | Mar -> 31
+    | Apr -> 30
+    | May -> 31
+    | Jun -> 30
+    | Jul -> 31
+    | Aug -> 31
+    | Sep -> 30
+    | Oct -> 31
+    | Nov -> 30
+    | Dec -> 31
+
+let precedingMonth month =
+    match month with
+        | Jan -> raise (ArgumentException("No month precedes January"))
+        | Feb -> Jan
+        | Mar -> Feb
+        | Apr -> Mar
+        | May -> Apr
+        | Jun -> May
+        | Jul -> Jun
+        | Aug -> Jul
+        | Sep -> Aug
+        | Oct -> Sep
+        | Nov -> Oct
+        | Dec -> Nov
+
+let rec firstOfMonth month year : int =
+    match month with
+    | Jan -> 1
+    | _ -> (firstOfMonth (precedingMonth month) year) + (daysInMonth (precedingMonth month) year)
---
+++ Puzzles.Tests.FSharp/TimeTests.fs
@@ -0,0 +1,42 @@
+﻿module Puzzles.Tests.FSharp.TimeTests
+
+open Time
+open NUnit.Framework
+
+[<Test>]
+let isLeapYearTest () =
+    Assert.IsFalse(Time.isLeapYear 1599)
+    Assert.IsTrue(Time.isLeapYear 1600)
+    Assert.IsFalse(Time.isLeapYear 1601)
+    Assert.IsFalse(Time.isLeapYear 1603)
+    Assert.IsTrue(Time.isLeapYear 1604)
+    Assert.IsFalse(Time.isLeapYear 1605)
+    
+    Assert.IsTrue(Time.isLeapYear 1896)
+    Assert.IsFalse(Time.isLeapYear 1897)
+    Assert.IsFalse(Time.isLeapYear 1898)
+    Assert.IsFalse(Time.isLeapYear 1899)
+    Assert.IsFalse(Time.isLeapYear 1900)
+    Assert.IsFalse(Time.isLeapYear 1901)
+    Assert.IsFalse(Time.isLeapYear 1902)
+    Assert.IsFalse(Time.isLeapYear 1903)
+    Assert.IsTrue(Time.isLeapYear 1904)
+
+[<Test>]
+let daysInYearTest () =
+    Assert.AreEqual(365, Time.daysInYear 1599)
+    Assert.AreEqual(366, Time.daysInYear 1600)
+    Assert.AreEqual(365, Time.daysInYear 1601)
+    Assert.AreEqual(365, Time.daysInYear 1603)
+    Assert.AreEqual(366, Time.daysInYear 1604)
+    Assert.AreEqual(365, Time.daysInYear 1605)
+    
+    Assert.AreEqual(366, Time.daysInYear 1896)
+    Assert.AreEqual(365, Time.daysInYear 1897)
+    Assert.AreEqual(365, Time.daysInYear 1898)
+    Assert.AreEqual(365, Time.daysInYear 1899)
+    Assert.AreEqual(365, Time.daysInYear 1900)
+    Assert.AreEqual(365, Time.daysInYear 1901)
+    Assert.AreEqual(365, Time.daysInYear 1902)
+    Assert.AreEqual(365, Time.daysInYear 1903)
+    Assert.AreEqual(366, Time.daysInYear 1904)
