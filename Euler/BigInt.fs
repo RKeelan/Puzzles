df8c115e4ea4510a3a336b3e677a596ca07a2cea
R.Keelan <R.Keelan@gmail.com>
2021-12-19 14:40:10 -0500

    Add BigInt
    - Multiply is the only supported operation

---
+++ Euler/BigInt.fs
@@ -0,0 +1,49 @@
+﻿module BigInt
+
+open System
+
+// The biggest power-of-ten radix that will fit insisde an int32
+[<Literal>]
+let INT_32_RADIX = 1000000000L
+
+// The biggest power-of-ten radix that will fit insisde an int64
+[<Literal>]
+let INT_64_RADIX = 1000000000000000000L
+
+type BigInt (componentsIn : list<int64>, radixIn : int64) =
+    let radix = radixIn
+    let formatString = new string((radix - 1L).ToString().ToCharArray() |> Array.map (fun c -> '0'))
+    let components = componentsIn
+
+    new(initialValue : int64, radix : int64) =
+        let initialComponents:list<int64> = initialValue |> List.unfold (fun n ->
+            if n = 0L then
+                None
+            else Some((n % radix), (n/radix)))
+        BigInt(initialComponents, radix)
+
+    override this.ToString() =
+        match List.rev(components) with
+        | [] -> ""
+        | head :: [] -> head.ToString()
+        | head :: tail -> head.ToString() + String.Join("", tail |> List.map (fun n -> n.ToString(formatString)))
+
+    member private this.multiplyInternal
+        (multiplicand : list<int64>)
+        (multiplier : int64)
+        (carryOver : int64)
+        : list<int64> =
+        match multiplicand with
+        // The least-significant element is the head of the list, so if we get to the end, return
+        // either nothing or the carry-over
+        | [] -> if carryOver = 0L then [] else [carryOver]
+        | head::tail ->
+            let newHead =
+                if (head * multiplier) >= radix then
+                    (head * multiplier + carryOver) % radix
+                else (head * multiplier + carryOver)
+            let newCarryOver = (head * multiplier) / radix
+            newHead :: (this.multiplyInternal tail multiplier newCarryOver)
+
+    member this.multiply (multiplier : int64) : BigInt =
+        new BigInt((this.multiplyInternal components multiplier 0L), radix)
---
+++ Puzzles.Tests.FSharp/BigIntTests.fs
@@ -0,0 +1,101 @@
+﻿module Puzzles.Tests.FSharp.BigIntTests
+
+open NUnit.Framework
+open BigInt
+
+[<Test>]
+let ctor () =
+    let mutable bigInt = BigInt(1L, 10L)
+    Assert.AreEqual("1", bigInt.ToString())
+
+    bigInt <- BigInt(11L, 10L)
+    Assert.AreEqual("11", bigInt.ToString())
+    
+    bigInt <- BigInt(1000L, BigInt.INT_32_RADIX)
+    Assert.AreEqual("1000", bigInt.ToString())
+    
+    bigInt <- BigInt(12345678901L, BigInt.INT_32_RADIX)
+    Assert.AreEqual("12345678901", bigInt.ToString())
+    
+    bigInt <- BigInt(12345678901L, BigInt.INT_64_RADIX)
+    Assert.AreEqual("12345678901", bigInt.ToString())
+    
+    bigInt <- BigInt(123456789987654L, BigInt.INT_64_RADIX)
+    Assert.AreEqual("123456789987654", bigInt.ToString())
+
+[<Test>]
+let toString () =
+    let mutable bigInt = BigInt(101L, 10L)
+    Assert.AreEqual("101", bigInt.ToString())
+
+    bigInt <- BigInt(1001L, 100L)
+    Assert.AreEqual("1001", bigInt.ToString())
+    
+    bigInt <- BigInt(10001L, 1000L)
+    Assert.AreEqual("10001", bigInt.ToString())
+    
+    bigInt <- BigInt(100001L, 10000L)
+    Assert.AreEqual("100001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000001L, 100000L)
+    Assert.AreEqual("1000001", bigInt.ToString())
+    
+    bigInt <- BigInt(10000001L, 1000000L)
+    Assert.AreEqual("10000001", bigInt.ToString())
+    
+    bigInt <- BigInt(100000001L, 10000000L)
+    Assert.AreEqual("100000001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000000001L, 100000000L)
+    Assert.AreEqual("1000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(10000000001L, 1000000000L)
+    Assert.AreEqual("10000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(100000000001L, 10000000000L)
+    Assert.AreEqual("100000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000000000001L, 100000000000L)
+    Assert.AreEqual("1000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(10000000000001L, 1000000000000L)
+    Assert.AreEqual("10000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(100000000000001L, 10000000000000L)
+    Assert.AreEqual("100000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000000000000001L, 100000000000000L)
+    Assert.AreEqual("1000000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(10000000000000001L, 1000000000000000L)
+    Assert.AreEqual("10000000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(100000000000000001L, 10000000000000000L)
+    Assert.AreEqual("100000000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000000000000000001L, 100000000000000000L)
+    Assert.AreEqual("1000000000000000001", bigInt.ToString())
+    
+    bigInt <- BigInt(1000000000000000001L, 10L)
+    Assert.AreEqual("1000000000000000001", bigInt.ToString())
+
+[<Test>]
+let multiply () =
+    let mutable bigInt = BigInt(6L, 10L)
+    bigInt <- bigInt.multiply(2L)
+    Assert.AreEqual("12", bigInt.ToString())
+        
+[<Test>]
+let double () =
+    let rec double (number : BigInt) (n : int64) =
+        let numberDoubled = number.multiply(2L)
+        match n with
+        | 1L -> numberDoubled
+        | _ -> double numberDoubled (n - 1L)
+    let bigInt = double (new BigInt(1L, BigInt.INT_32_RADIX)) 1000L
+    let bigResult = "107150860718626732094842504906000181056140481170553360744375038837035105112" +
+                    "493612249319837881569585812759467291755314682518714528569231404359845775746" +
+                    "985748039345677748242309854210746050623711418779541821530464749835819412673" +
+                    "98767559165543946077062914571196477686542167660429831652624386837205668069376"
+    Assert.AreEqual(bigResult, bigInt.ToString())
+
