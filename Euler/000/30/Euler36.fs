f76462e204136e33c60c7d82b6dcc1239cc5643f
R.Keelan <R.Keelan@gmail.com>
2021-12-27 15:21:20 -0500

    Add Euler36

---
+++ Euler/000/30/Euler36.fs
@@ -0,0 +1,38 @@
+﻿module Euler36
+
+(*
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+*)
+
+open System
+
+let isDoubleBasePalindrome (n:int) =
+    let isBaseTenPalindrome = Util.isPalindrome (n.ToString())
+    let isBaseTwoPalinrome = Util.isPalindrome (Convert.ToString(n,2))
+    //printfn $"{n} in binary is {Convert.ToString(n,2)}"
+    isBaseTenPalindrome && isBaseTwoPalinrome
+
+let doubleBasePalindromes n =
+    seq {1..n}
+    |> Seq.filter (fun n -> not (n.ToString().EndsWith('0')))
+    |> Seq.filter isDoubleBasePalindrome
+
+//[<EntryPoint>]
+let main argv =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    
+    //printfn $"585 is a double-base palindrome: {isDoubleBasePalindrome 585}"
+
+    let N = 999999
+    let palindromes = doubleBasePalindromes N |> Seq.toList
+    let sum = palindromes |> List.sum
+    printfn "%s" (String.Join('\n', palindromes))
+    printfn $"The sum of the {palindromes.Length} double-base palindromes under {(N+1):N0} is {sum}"
+    
+    stopWatch.Stop()
+    printfn $"Execution time: {stopWatch.Elapsed.TotalMilliseconds:N0} ms." 
+    0
