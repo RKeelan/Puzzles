f59e98cf14f164a96a242624127487035ac7e058
R.Keelan <R.Keelan@gmail.com>
2022-01-03 14:44:22 -0500

    Add Combinatronics module

---
+++ Euler/Combinatronics.fs
@@ -0,0 +1,13 @@
+﻿module Combinatronics
+
+open System
+    
+let rec words alphabet length =
+    seq {
+        match length with
+        | 0 -> yield []
+        | n ->
+            for letter in alphabet do
+                for word in (words alphabet (length-1)) do
+                    yield letter::word
+    }
---
+++ Puzzles.Tests.FSharp/CombinatronicsTests.fs
@@ -0,0 +1,40 @@
+﻿module Puzzles.Tests.FSharp.CombinatronicsTests
+
+open System
+open NUnit.Framework
+
+open System
+
+[<Test>]
+let words () =
+    let abAlphabet = set ['A';'B']
+
+    let mutable expected = seq {
+        ['A'];
+        ['B']
+    }
+    let mutable actual = Combinatronics.words abAlphabet 1
+    Assert.AreEqual(expected, actual)
+    
+    expected <- seq {
+        ['A';'A'];
+        ['A';'B'];
+        ['B';'A'];
+        ['B';'B']
+    }
+    actual <- Combinatronics.words abAlphabet 2
+    Assert.AreEqual(expected, actual)
+    Assert.AreEqual(expected, actual)
+    
+    expected <- seq {
+        ['A';'A';'A'];
+        ['A';'A';'B'];
+        ['A';'B';'A'];
+        ['A';'B';'B'];
+        ['B';'A';'A'];
+        ['B';'A';'B'];
+        ['B';'B';'A']
+        ['B';'B';'B']
+    }
+    actual <- Combinatronics.words abAlphabet 3
+    Assert.AreEqual(expected, actual)
